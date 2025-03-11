bf472a86c8e05fd8080ebc470ae74b9c22e80b06
Vinay R K <Vinaykabbalageri098@gmail.com>
2025-03-12 01:45:18 +0530

    Binary Search Cocepts

---
+++ ceil(bs).java
@@ -0,0 +1,26 @@
+package practice;
+
+public class binary {
+    public static void main(String[] args) {
+        int arr[]={1,3,4,6,8,9,11};
+        int target=7;
+        int ans=ceil(arr,target);
+        System.out.println("the ceil value is"+"   "+ans);
+    }
+    static int ceil(int arr[],int target){
+        int start=arr[0];
+        int end=arr[arr.length-1];
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target>arr[mid]){
+                start=mid+1;
+            } else if (target<arr[mid]) {
+                end=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return start;
+    }
+}
